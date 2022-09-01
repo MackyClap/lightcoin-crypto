cc5d9d6d9b4503631c5c5ffbed6eed86c1a56eb9
MackyClap <martimac2320@gmail.com>
2022-08-31 23:03:05 -0400

    left off at missing feature 1

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount,account){
     this.amount = amount;
+    this.account = account;
   }
 
-  commit() {
-    balance -= this.amount;
+    commit () {
+      this.account.balance += this.value;
+    }
+  }
+
+
+
+class Withdrawal extends Transaction{
+
+  get value() {
+    return -this.amount;
   }
 
 }
 
+class Deposit extends Transaction{
+
+  get value() {
+    return this.amount;
+  }
+}
+
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+    this.transactions = [];
+  }
+  
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
